cc10b4aae7beee9d272aee13a1c4bea86822334f
vollram <128322069+vollram@users.noreply.github.com>
2023-08-20 19:30:28 +0200

    1 file modified

---
+++ projects.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="tab" id="tab" @click="animateTab">
+      <a href="#projects">Projects</a>
+    </div>
+    <div class="content">
+      <h1>Luca :)</h1>
+    </div>
+  </div>
+</template>
